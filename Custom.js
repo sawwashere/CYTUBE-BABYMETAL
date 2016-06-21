6e74523e6c8a4786c8aa8ddb72a2b11c7a0d0802
sawwashere <sawwashere@users.noreply.github.com>
2016-06-21 19:43:01 -0400

    moving scroll-feature to before rightpane

--- Custom.js
+++ Custom.js
@@ -91,7 +91,7 @@ $("#modli").append($("#modflair"));
 $("#headbottom").append($("#mediarefresh"));
 $("#mediarefresh").text("Reload Video Player");
 $("#videoinfo").after($("#rightpane"));
-$("#videoinfo").after($("#scroll-feature"));
+$("#rightpane").before($("#scroll-feature"));
 $("#rightpane-inner").prepend("<div id='mediabuttons'></div>");
 $("#rightpane-inner").addClass("section");
 $("#mediarefresh").addClass("btn btn-sm btn-default OLB");
