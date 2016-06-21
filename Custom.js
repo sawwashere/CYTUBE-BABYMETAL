cd5dd4d417d18b13e8bf7a3162f4b907ab0c2fe6
sawwashere <sawwashere@users.noreply.github.com>
2016-06-21 19:11:25 -0400

    moving scroll-feature after videoinfo append call to try to fix ordering

--- Custom.js
+++ Custom.js
@@ -45,10 +45,10 @@ $("#userlisttoggle").after($("#emotelistbtn"));
 $("#main").after($("#drinkbarwrap"));
 $("#main").after($("#motdrow"));
 $("#main").after("<div id='videoinfo' class='section'></div>");//create box to contain video title, description, and playlist options.
-$("#videoinfo").after($("#scroll-feature"));
 
 
 $("#videoinfo").append("<div class='textheader'></div><div id='videoinfohead'><span id='addedbyTEXT'>Queued by <span id='addedby'></span></span><div id='headbottom'><div id='headright'><div id='ss7time' title='--:--'>0:00</div><div id='videolength'></div><div id='progbar'></div></div></div></div><div id='videoopts'></div>");
+$("#videoinfo").after($("#scroll-feature"));
 $(".navbar-header").after($("#currenttitle")); //move video title below video player
 $("#headbottom").append("<button id='addmedia' title='Add Media' class='headbtn headbtnleft'></button>");
 $("#headbottom").append($("#newpollbtn"));
