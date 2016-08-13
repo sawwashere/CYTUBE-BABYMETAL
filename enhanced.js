9f20d2e4f68e6171e3afbada68cb38ba777c6a03
sawwashere <sawwashere@users.noreply.github.com>
2016-08-13 14:10:27 -0400

    changing avatar to prepend to parent of parent of username to prevent being added on every line with timestamp

--- enhanced.js
+++ enhanced.js
@@ -1122,7 +1122,7 @@ window.cytubeEnhanced.addModule('chatAvatars', function (app) {
             if ((window.findUserlistItem(data.username) != null) && (window.findUserlistItem(data.username).data('profile').image != "") && (app.userConfig.get('avatarsMode') != false)) {
                 var $avatar = $("<img>").attr("src", window.findUserlistItem(data.username).data('profile').image)
                     .addClass(avatarCssClasses)
-                    .prependTo(div.find('.timestamp').parent());
+                    .prependTo(div.find('.username').parent().parent());
 
                 if (app.userConfig.get('avatarsMode') == 'big') {
                     div.find('.username').css('display', 'none');
